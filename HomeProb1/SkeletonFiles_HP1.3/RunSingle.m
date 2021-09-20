9ca0bf7ae2ce4224b87f2dd5c0732d316bf0b2eb
Stefan Chan <chanstefan98@gmail.com>
2021-09-20 17:05:17 +0200

    Found near optimal mutation rate

--- HomeProb1/SkeletonFiles_HP1.3/RunSingle.m
+++ HomeProb1/SkeletonFiles_HP1.3/RunSingle.m
@@ -10,8 +10,8 @@ numberOfVariables = 2;  	   % Do NOT change
 tournamentSize = 2;                % Changes allowed
 tournamentProbability = 0.75;      % Changes allowed (= pTour)
 crossoverProbability = 0.8;        % Changes allowed (= pCross)
-mutationProbability = 0.02;        % Changes allowed. (Note: 0.02 <=> 1/numberOfGenes)
-numberOfGenerations = 2000;        % Changes allowed.
+mutationProbability = 0.1;        % Changes allowed. (Note: 0.02 <=> 1/numberOfGenes)
+numberOfGenerations = 300;        % Changes allowed.
 
 [maximumFitness, bestVariableValues] = RunFunctionOptimization(populationSize, numberOfGenes, numberOfVariables, maximumVariableValue, tournamentSize, ...
                                        tournamentProbability, crossoverProbability, mutationProbability, numberOfGenerations);
