4ce8894b32d41a8ecbfe9a3c6eea06a5d198b64e
Stefan Chan <chanstefan98@gmail.com>
2021-09-20 16:46:23 +0200

    Fixed squaring mistake in evaluation formula

--- HomeProb1/SkeletonFiles_HP1.3/EvaluateIndividual.m
+++ HomeProb1/SkeletonFiles_HP1.3/EvaluateIndividual.m
@@ -6,11 +6,11 @@ function fitness = EvaluateIndividual(x)
     x_2 = x(2);
 
     firstTerm = (1.5 - x_1 + (x_1 * x_2)).^2;
-    secondTerm = (2.25 - x_1 + (x_1 * x_2).^2).^2;
-    thirdTerm = (2.625 - x_1 + (x_1 * x_2).^3).^2;
+    secondTerm = (2.25 - x_1 + (x_1 * (x_2).^2)).^2;
+    thirdTerm = (2.625 - x_1 + (x_1 * (x_2).^3)).^2;
 
     g_x = firstTerm + secondTerm + thirdTerm;
 
-    fitness = 1 / g_x;
+    fitness = 1.0 / g_x;
 
 end
