f2a563895df1913a66078a38dd0d2ef4a98616ac
Stefan Chan <chanstefan98@gmail.com>
2021-10-12 15:22:39 +0200

    Cleanup

--- HomeProb2/Main.m
+++
@@ -1,6 +0,0 @@
-cityLocation = LoadCityLocations;
-nCities = size(cityLocation,1);
-path = randperm(nCities);                
-tspFigure = InitializeTspPlot(cityLocation,[0 20 0 20]); 
-connection = InitializeConnections(cityLocation); 
-PlotPath(connection,cityLocation,path);     
